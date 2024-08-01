be0879677e648e7e69185082210be61370a406f9
leon legrand <assougnonleon@gmail.com>
2024-08-01 05:17:27 +0000

    configuration du gitignore 1

---
+++ public/js/depenses-list.js
@@ -0,0 +1,308 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(self, function() {
+return /******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!***************************************!*\
+  !*** ./resources/js/depenses-list.js ***!
+  \***************************************/
+
+
+$(function () {
+  var dt_user_table = $('.depenses-list-table');
+  var depnseCreate = baseUrl + 'add/depense';
+  $.ajaxSetup({
+    headers: {
+      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+  });
+  if (dt_user_table.length) {
+    var dt_user = dt_user_table.DataTable({
+      processing: true,
+      serverSide: true,
+      ajax: {
+        url: baseUrl + 'show/depenses'
+      },
+      columns: [
+      // columns according to JSON
+      {
+        data: ''
+      }, {
+        data: 'id'
+      }, {
+        data: 'montant'
+      }, {
+        data: 'banque_id'
+      }, {
+        data: 'description'
+      }, {
+        data: 'actions'
+      }],
+      columnDefs: [{
+        // For Responsive
+        className: 'control',
+        searchable: false,
+        orderable: false,
+        responsivePriority: 2,
+        targets: 0,
+        render: function render(data, type, full, meta) {
+          return '';
+        }
+      }, {
+        searchable: true,
+        orderable: false,
+        targets: 1,
+        render: function render(data, type, full, meta) {
+          return "<span>".concat(full.fake_id, "</span>");
+        }
+      }, {
+        // User full name
+        targets: 2,
+        responsivePriority: 3,
+        render: function render(data, type, full, meta) {
+          var $montant = full['montant'];
+          return $montant;
+        }
+      }, {
+        targets: 3,
+        render: function render(data, type, full, meta) {
+          var $description = full['description'];
+          return '<span class="">' + $description + '</span>';
+        }
+      }, {
+        // User telephone
+        targets: 4,
+        responsivePriority: 4,
+        render: function render(data, type, full, meta) {
+          var $banque = full['banque'];
+          return "".concat($banque);
+        }
+      }, {
+        // Actions
+        targets: -1,
+        title: 'Actions',
+        searchable: false,
+        orderable: false,
+        render: function render(data, type, full, meta) {
+          return '<div class="d-inline-block text-nowrap">' + "<button class=\"btn btn-sm btn-icon edit-record\" data-id=\"".concat(full['id'], "\" ><a href=\"edit/depense/").concat(full['id'], "\"><i class=\"ti ti-edit\"></i></a></button>") + "<button class=\"btn btn-sm btn-icon delete-record\" data-id=\"".concat(full['id'], "\"><i class=\"ti ti-trash\"></i></button>") + '</div>';
+        }
+      }],
+      order: [[2, 'desc']],
+      dom: '<"row mx-2"' + '<"col-md-2"<"me-3"l>>' + '<"col-md-10"<"dt-action-buttons text-xl-end text-lg-start text-md-end text-start d-flex align-items-center justify-content-end flex-md-row flex-column mb-3 mb-md-0"fB>>' + '>t' + '<"row mx-2"' + '<"col-sm-12 col-md-6"i>' + '<"col-sm-12 col-md-6"p>' + '>',
+      language: {
+        sLengthMenu: '_MENU_',
+        search: '',
+        searchPlaceholder: 'Rechercher..'
+      },
+      // Buttons with Dropdown
+      buttons: [{
+        extend: 'collection',
+        className: 'btn btn-label-primary dropdown-toggle mx-3',
+        text: '<i class="ti ti-logout rotate-n90 me-2"></i>Exporter',
+        buttons: [{
+          extend: 'print',
+          title: 'Utilisateurs',
+          text: '<i class="ti ti-printer me-2" ></i>Imprimer',
+          className: 'dropdown-item',
+          exportOptions: {
+            columns: [2, 3, 4, 5],
+            // prevent avatar to be print
+            format: {
+              body: function body(inner, coldex, rowdex) {
+                if (inner.length <= 0) return inner;
+                var el = $.parseHTML(inner);
+                var result = '';
+                $.each(el, function (index, item) {
+                  if (item.classList !== undefined && item.classList.contains('user-name')) {
+                    result = result + item.lastChild.textContent;
+                  } else result = result + item.innerText;
+                });
+                return result;
+              }
+            }
+          },
+          customize: function customize(win) {
+            //customize print view for dark
+            $(win.document.body).css('color', config.colors.headingColor).css('border-color', config.colors.borderColor).css('background-color', config.colors.body);
+            $(win.document.body).find('table').addClass('compact').css('color', 'inherit').css('border-color', 'inherit').css('background-color', 'inherit');
+          }
+        }, {
+          extend: 'csv',
+          title: 'Utilisateurs',
+          text: '<i class="ti ti-file-text me-2" ></i>Csv',
+          className: 'dropdown-item',
+          exportOptions: {
+            columns: [2, 3, 4, 5],
+            // prevent avatar to be print
+            format: {
+              body: function body(inner, coldex, rowdex) {
+                if (inner.length <= 0) return inner;
+                var el = $.parseHTML(inner);
+                var result = '';
+                $.each(el, function (index, item) {
+                  if (item.classList.contains('user-name')) {
+                    result = result + item.lastChild.textContent;
+                  } else result = result + item.innerText;
+                });
+                return result;
+              }
+            }
+          }
+        }, {
+          extend: 'excel',
+          title: 'Utilisateurs',
+          text: '<i class="ti ti-file-spreadsheet me-2"></i>Excel',
+          className: 'dropdown-item',
+          exportOptions: {
+            columns: [2, 3, 4, 5],
+            // prevent avatar to be display
+            format: {
+              body: function body(inner, coldex, rowdex) {
+                if (inner.length <= 0) return inner;
+                var el = $.parseHTML(inner);
+                var result = '';
+                $.each(el, function (index, item) {
+                  if (item.classList.contains('user-name')) {
+                    result = result + item.lastChild.textContent;
+                  } else result = result + item.innerText;
+                });
+                return result;
+              }
+            }
+          }
+        }, {
+          extend: 'pdf',
+          title: 'Utilisateurs',
+          text: '<i class="ti ti-file-text me-2"></i>Pdf',
+          className: 'dropdown-item',
+          exportOptions: {
+            columns: [2, 3, 4, 5],
+            // prevent avatar to be display
+            format: {
+              body: function body(inner, coldex, rowdex) {
+                if (inner.length <= 0) return inner;
+                var el = $.parseHTML(inner);
+                var result = '';
+                $.each(el, function (index, item) {
+                  if (item.classList.contains('user-name')) {
+                    result = result + item.lastChild.textContent;
+                  } else result = result + item.innerText;
+                });
+                return result;
+              }
+            }
+          }
+        }, {
+          extend: 'copy',
+          title: 'Utilisateur',
+          text: '<i class="ti ti-copy me-1" ></i>Copy',
+          className: 'dropdown-item',
+          exportOptions: {
+            columns: [2, 3, 4, 5],
+            // prevent avatar to be copy
+            format: {
+              body: function body(inner, coldex, rowdex) {
+                if (inner.length <= 0) return inner;
+                var el = $.parseHTML(inner);
+                var result = '';
+                $.each(el, function (index, item) {
+                  if (item.classList.contains('user-name')) {
+                    result = result + item.lastChild.textContent;
+                  } else result = result + item.innerText;
+                });
+                return result;
+              }
+            }
+          }
+        }]
+      }, {
+        text: '<i class="ti ti-plus me-0 me-sm-1"></i><span class="d-none d-sm-inline-block add-depense">Ajouter une dépense</span>',
+        className: 'add-new btn btn-primary'
+      }],
+      // For responsive popup
+      responsive: {
+        details: {
+          display: $.fn.dataTable.Responsive.display.modal({
+            header: function header(row) {
+              var data = row.data();
+              return 'Details de ' + data['prenom'];
+            }
+          }),
+          type: 'column',
+          renderer: function renderer(api, rowIdx, columns) {
+            var data = $.map(columns, function (col, i) {
+              return col.title !== '' // ? Do not show row in modal popup if title is blank (for check box)
+              ? '<tr data-dt-row="' + col.rowIndex + '" data-dt-column="' + col.columnIndex + '">' + '<td>' + col.title + ':' + '</td> ' + '<td>' + col.data + '</td>' + '</tr>' : '';
+            }).join('');
+            return data ? $('<table class="table"/><tbody />').append(data) : false;
+          }
+        }
+      }
+    });
+  }
+  $('.add-depense').on('click', function () {
+    window.location.href = baseUrl + "add/depense";
+  });
+  $(document).on('click', '.delete-record', function () {
+    var depense_id = $(this).data('id');
+
+    // sweetalert for confirmation of delete
+    Swal.fire({
+      title: 'êtes vous sur ?',
+      text: "La suppression est irreversible !",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Oui , Je supprime !',
+      customClass: {
+        confirmButton: 'btn btn-primary me-3',
+        cancelButton: 'btn btn-label-secondary'
+      },
+      buttonsStyling: false
+    }).then(function (result) {
+      if (result.value) {
+        // delete the data
+        $.ajax({
+          type: 'GET',
+          url: "".concat(baseUrl, "delete/depense/").concat(depense_id),
+          success: function success() {
+            dt_user.draw();
+          },
+          error: function error(_error) {
+            console.log(_error);
+          }
+        });
+
+        // success sweetalert
+        Swal.fire({
+          icon: 'success',
+          title: 'Supprimer!',
+          text: 'La Dépense  a bien été Supprimée !',
+          customClass: {
+            confirmButton: 'btn btn-success'
+          }
+        });
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire({
+          title: 'Annuler',
+          text: 'La Dépense n\'a pas été supprimé!',
+          icon: 'error',
+          customClass: {
+            confirmButton: 'btn btn-success'
+          }
+        });
+      }
+    });
+  });
+});
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
